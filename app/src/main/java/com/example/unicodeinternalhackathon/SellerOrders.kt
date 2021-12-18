0eb7d60301033d729c2f880bc4488895bd174758
Yash <yashr10102@gmail.com>
2021-12-18 17:11:28 +0530

    misc details

---
+++ app/src/main/java/com/example/unicodeinternalhackathon/SellerOrders.kt
@@ -0,0 +1,11 @@
+package com.example.unicodeinternalhackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerOrders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_orders)
+    }
+}
